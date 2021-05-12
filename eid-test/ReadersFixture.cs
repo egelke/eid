6aac66bd1aad0bb4080d76acbdc9fba0d68e9cbc
A434463 <bryan.brouckaert.ext@atos.net>
2021-05-12 11:59:34 +0200

    Upgrade to .net 4.6.1

---
+++ eid-test/ReadersFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egelke.Eid.Client.Test
+{
+    public class ReadersFixture : IDisposable
+    {
+        public Readers Readers { get; }
+
+        public ReadersFixture()
+        {
+            Readers = new Readers(ReaderScope.User);
+        }
+
+        public void Dispose()
+        {
+            Readers.Dispose();
+        }
+    }
+}
